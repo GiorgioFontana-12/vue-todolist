84a504cd0c272a4b7fddce3833f5338472142dcb
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-10 17:07:53 +0200

    feat addTask

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-           
+            newTask: {
+                task: "",
+                done: false
+            },
+            taskList: []
         }
     },
+
     methods: {
+        addNewTask(){
+            let addedTask = { ...this.newTask };
+            this.taskList.unshift(addedTask);
+            this.newTask.task = "";
+        }
 
     },
 
